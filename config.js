7c4ee0d6a17352c5fcedf162f5d0089dfe0c17c8
xsthunder <xsthunder@outlook.com>
2019-09-10 22:28:00 +0800

    update calendar id

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const path = require('path')
 const timezone = 'UTC+08:00';
 module.exports = {
     "termStartdateOnMonday": new Date('2019-09-02T00:00:00'),
-    "calendarId": "hquf9lap6st1p9cgv5grtomjik@group.calendar.google.com",
+    "calendarId": "3fgidsiuflumfjpgc5sinhtrfc@group.calendar.google.com",
     timezone,
 }
